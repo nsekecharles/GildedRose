48621cdea51476f518542f3018a3f0455bdb9a30
charles nseke <nsekecharles@gmail.com>
2017-06-01 20:29:42 +0200

    indentation

--- src/main/java/kata/gildedrose/itemstransformers/ItemMutator.java
+++ src/main/java/kata/gildedrose/itemstransformers/ItemMutator.java
@@ -3,33 +3,33 @@ package kata.gildedrose.itemstransformers;
 import kata.gildedrose.legacy.Item;
 
 public interface ItemMutator {
-	
-	default void mutate(Item item) {
-    	updateSellInOf(item);
-    	decreaseQualityOf(item);
-    	if (item.getSellIn() < 0) {
-    		decreaseQualityOf(item);
-    	}
-	}
-	
-	default void decreaseQualityOf(Item item) {
-		if (item.getQuality() > 0) {
-			item.setQuality(item.getQuality() - 1);
-		}
-	}
 
-	default void increaseQualityOf(Item item) {
-		if(item.getQuality() < 50) {
-		    item.setQuality(item.getQuality() + 1);
-		}
+    default void mutate(Item item) {
+	updateSellInOf(item);
+	decreaseQualityOf(item);
+	if (item.getSellIn() < 0) {
+	    decreaseQualityOf(item);
 	}
-	
-	default void setQualityToZeroFor(Item item) {
-		item.setQuality(0);
+    }
+
+    default void decreaseQualityOf(Item item) {
+	if (item.getQuality() > 0) {
+	    item.setQuality(item.getQuality() - 1);
 	}
-	
-	default void updateSellInOf(Item item) {
-		item.setSellIn(item.getSellIn() - 1);
+    }
+
+    default void increaseQualityOf(Item item) {
+	if(item.getQuality() < 50) {
+	    item.setQuality(item.getQuality() + 1);
 	}
+    }
+
+    default void setQualityToZeroFor(Item item) {
+	item.setQuality(0);
+    }
+
+    default void updateSellInOf(Item item) {
+	item.setSellIn(item.getSellIn() - 1);
+    }
 
 }
